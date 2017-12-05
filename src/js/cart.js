0b98e94a0b8a3e2dd3f70d5a77991fb739406228
bkanya <bkanya@atl.yearup.org>
2017-12-05 09:22:15 -0500

    More stuff

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,18 +1,5 @@
 'use strict';
 
-class Product {
-  constructor(name, price, quantity, description, imageUrl)
-
-  {
-    this.name = name;
-    this.price = price;
-    this.quantity = quantity;
-    this.description = description;
-    this.imageUrl = imageUrl;
-  }
-
-}
-
 class Cart {
   constructor(taxRate, subtotal, paymentMethod) {
     this.taxRate = taxRate;
---
+++ src/js/product.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class Product {
+  constructor(name, price, quantity, description, imageUrl)
+
+  {
+    this.name = name;
+    this.price = price;
+    this.quantity = quantity;
+    this.description = description;
+    this.imageUrl = imageUrl;
+  }
+
+}
